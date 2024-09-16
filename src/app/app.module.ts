bb1e4c131815ae64e0b5ba8c3980be8035883528
jennyfaye <payeeehh@gmail.com>
2024-09-16 23:40:49 +0800

    Upload Angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { HomepageComponent } from "./components/homepage/homepage.component";
+import { LandingpageComponent } from "./components/landingpage/landingpage.component";
+
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'signup', component: SignupComponent},
+  { path: '', component: HomepageComponent},
+  { path: 'landingpage', component: LandingpageComponent},
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+],
+
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    HomepageComponent,
+    LandingpageComponent,
+  ],
+
+  providers: [
+  ],
+
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule { }
